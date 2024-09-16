0f26fac27567d2b1923070d2352c2f7aec1d1163
santfan <santfan@mail.ru>
2024-09-16 17:36:21 +0300

    Решение задачи переменные

---
+++ variables.py
@@ -0,0 +1,7 @@
+homework_count = 12
+hour_spent_count = 1.5
+coruse_name = 'Python'
+
+print('Курс:', coruse_name, ', всего задач:', homework_count, ', затрачено часов: ',
+      hour_spent_count, ', среднее время выполнения',
+      hour_spent_count / homework_count, 'часов')
